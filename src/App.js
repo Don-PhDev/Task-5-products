e7807852ac98ac74fd8f3c39983c2d881d02d373
Raymond <raymond@morphos.is>
2022-02-10 17:36:31 +0800

    Initialize Options component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import "./App.css";
+import Options from "./components/Options";
+import Summary from "./components/Summary";
+import Table from "./components/Table";
 
 function App() {
   return (
-    <div className="app">Hello Earth!</div>
+    <div className="app">
+      <h1>Products</h1>
+      <Options />
+      <Summary />
+      <Table />
+    </div>
   );
 }
 
---
+++ src/components/Options/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Options() {
+  return <button>Options</button>;
+}
+
+export default Options;
