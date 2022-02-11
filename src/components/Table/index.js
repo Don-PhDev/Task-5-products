d17e229178dd0f4d0d274b7ac66f87f37cfe3b39
Raymond <raymond@philippinedev.com>
2022-02-11 14:21:00 +0800

    Refactor Table component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,51 +1,60 @@
 import { useState } from "react";
-import MockData from "../../MOCK_DATA.json";
 
-function Table() {
-  const [data, setData] = useState(MockData);
-  const [order, setOrder] = useState("ASC");
-  const sorting = (col) => {
-    if (order === "ASC") {
-      const sorted = [...data].sort((a, b) =>
-        a[col]?.toLowerCase() > b[col]?.toLowerCase() ? 1 : -1
-      );
-      setData(sorted);
-      setOrder("DSC");
-    } else {
-      setOrder("ASC");
+export default props => {
+  const fields = [
+    'id',
+    'category',
+    'name',
+    'price',
+    'manufacturer',
+    'production_date',
+  ]
+
+  const [data, setData] = useState(props.data);
+  const [order, setSort] = useState([
+    { field: fields[0], direction: "" },
+    { field: fields[1], direction: "" },
+    { field: fields[2], direction: "" },
+    { field: fields[3], direction: "" },
+    { field: fields[4], direction: "" },
+    { field: fields[5], direction: "" },
+  ])
+
+  const sorting = field => {
+    const setdirection = old => old === "ASC" ? "DESC" : "ASC"
+
+    setSort(order.map(row => field === row.field ? ({
+      ...row, direction: setdirection(row.direction)
+    }) : row))
+
+    const dir = order.find(x => x.field === field).direction
+
+    const sorter = (a, b) => {
+      const aPath = dir === "ASC" ? 1 : -1
+      return a[field] >= b[field] ? aPath : (aPath * -1)
     }
-  };
+
+    setData(data.sort(sorter))
+  }
+
+  const generatePriceData = () => [...new Set(data.map((product) => product.price))]
+
+  const clickHandler = idx => () => sorting(fields[idx])
+
+  const rowStyle = id => id === props.highest.id ? { background: "red" } : {}
 
   return (
     <div>
-      <table className="table table-bordered table-hover table-striped">
-        <thead>
-          <tr onClick={() => sorting()}>
-            <th>#</th>
-            <th>Category</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Manufacturer</th>
-            <th>Production Date</th>
-          </tr>
+      <table className="table table-bordered table-hover table-striped mt-3">
+        <thead className="table-dark">
+          <tr>{ fields.map((x, idx) => <th onClick={ clickHandler(idx) }>{ fields[idx] }</th>) }</tr>
         </thead>
-        <tbody>
-          {data.map((d) => {
-            return (
-              <tr key={d.id}>
-                <td>{d.id}</td>
-                <td>{d.category}</td>
-                <td>{d.name}</td>
-                <td>${d.price}</td>
-                <td>{d.manufacturer}</td>
-                <td>{d.production_date}</td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <tbody>{ data.map((row, idx) => (
+          <tr key={ row.id } style={ rowStyle(row.id) }>{
+            fields.map((x, idx) => <td>{ row[fields[idx]] }</td>)
+          }</tr>
+        )) }</tbody>
       </table>
     </div>
-  );
+  )
 }
-
-export default Table;
