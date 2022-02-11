416552f0c3ace01ed4f24c2c1ea4d13c3e9b0658
Raymond <raymond@philippinedev.com>
2022-02-11 14:24:06 +0800

    Initialize Options component

--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+function Options({ prices }) {
+  return (
+    <div className="options">
+      <h2>Filtering</h2>
+      <div className="col">
+        <button className="btn btn-primary mb-2">Options</button>
+      </div>
+      <div className="col-sm-12 my-2">
+        <label htmlFor="category">Category</label>
+        <input type="text" className="form-control" id="category" />
+      </div>
 
-function Options() {
-  return <button>Options</button>;
+      <div className="col-sm-12 my-2">
+        <label htmlFor="manufacturer">Manufacturer</label>
+        <input type="text" className="form-control" id="manufacturer" />
+      </div>
+
+      <div className="col-sm-12 my-2">
+        <label htmlFor="price">Price</label>
+        <select className="form-control" id="price">
+          <option value="">Select</option>
+          {prices.map(price => {
+            return <option value={price}>{price}</option>;
+          })}
+        </select>
+      </div>
+        <button className="btn btn-secondary mr-2">Reset</button>
+        <button className="btn btn-success m-2">Apply</button>
+    </div>
+  );
 }
 
 export default Options;
