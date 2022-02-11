77a1e6fd24bb8d2dd49bbb7f2f7869417216f4e5
Raymond <raymond@philippinedev.com>
2022-02-11 14:24:36 +0800

    Refactor Summary component

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,31 +1,40 @@
-import { useState } from "react";
-import MockData from "../../MOCK_DATA.json";
+function Summary({ data, highest, lowest }) {
+  console.log({ highest})
+  const totalCost = () => {
+    return data.reduce((sum, a) => sum + a.price, 0)
+  }
 
-function Summary() {
-  const [data, setData] = useState(MockData);
+  const averagePrice = () => {
+    if (data.length === 0) return 0.0
+    return totalCost() / data.length
+  }
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
 
   return (
-    <div>
-      <h3>Total quantity: {data.length}</h3>
-      <h3>Total cost: {}</h3>
-      <h3>Average price: {}</h3>
-      <h3>
-        Most expensive:{" $"}
-        {Math.max(
-          ...data.map((d) => {
-            return d.price;
-          })
-        )}
-      </h3>
-      <h3>
-        Cheapest:{" $"}
-        {Math.min(
-          ...data.map((d) => {
-            return d.price;
-          })
-        )}
-      </h3>
-    </div>
+    <table className="table table-bordered mt-2 text-center">
+      <thead>
+        <tr>
+          <th><h3>Total quantity</h3></th>
+          <th><h3>Total cost</h3></th>
+          <th><h3>Average price</h3></th>
+          <th><h3>Most expensive</h3></th>
+          <th><h3>Cheapest</h3></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{data.length}</td>
+          <td>{formatter.format(totalCost())}</td>
+          <td>{formatter.format(averagePrice())}</td>
+          <td>{highest.name} {formatter.format(highest.price)}</td>
+          <td>{lowest.name} {formatter.format(lowest.price)}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
